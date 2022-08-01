0c920984ae9cbb2b6574ba63fcf41b3785d25413
nk400 <navkr007@gmail.com>
2022-08-01 17:02:24 +0530

    REFACTORED: Validation done in Address Book dto for all fields

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -2,6 +2,8 @@ package com.bridgelabz.addressbookapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -10,11 +12,17 @@ public class AddressBookDTO {
     public String firstName;
     @Pattern(regexp = "^[A-Z]{1}[A-Za-z]{1,}$", message = "Invalid Last Name")
     public String lastName;
+    @Pattern(regexp = "[0-9]{2}\\s[1-9][0-9]{9}$", message = "Invalid Phone number")
     public String phoneNumber;
+    @Pattern(regexp = "[a-zA-Z0-9][a-zA-Z-0-9_+]*([.][a-zA-Z0-9]+)?[@][a-zA-Z-0-9]+[.][a-z]{2,4}([.][a-zA-Z]{2,4})?", message = "Invalid Last Name")
     public String email;
+    @NotEmpty(message = "address cannot be null")
     public String address;
+    @NotEmpty(message = "city cannot be null")
     public String city;
+    @NotEmpty(message = "state cannot be null")
     public String state;
+    @NotNull(message = "zip cannot be null")
     public int zip;
 
     public AddressBookDTO(String firstName, String lastName, String phoneNumber, String email,
